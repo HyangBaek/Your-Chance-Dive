9d95852d5e24164af7ea1439a008313000e8e4aa
HyangBaek <22460026@st.yc.ac.kr>
2024-11-19 11:25:37 +0900

    03_front_header_change class to className and add search icon

--- pjt_01_ucd/frontend/src/components/Header.js
+++ pjt_01_ucd/frontend/src/components/Header.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import style from './style/Header.module.css';
+import searchIcon from '../images/searchicon.png'; // 이미지 파일 임포트
 
 const Header = ({ user, setUser }) => {
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -12,6 +15,10 @@ const Header = ({ user, setUser }) => {
     }
   }, [setUser]);
 
+  const handleGoToMyPage = () => {
+    navigate('/mypage');
+  };
+
   const handleLogout = () => {
     setUser(null);
     localStorage.removeItem('token');
@@ -19,18 +26,16 @@ const Header = ({ user, setUser }) => {
 
   return (
     <header className="header">
-      <nav class="w3-top">
-        <div class="w3-bar w3-white w3-padding w3-card">
-          <div class="w3-col s4">
-            <a href="/" class="w3-bar-item w3-button"><b>숨참고</b> <span class="w3-text-gray">보물 같은 중고 거래의 깊이 있는 발견</span></a>
+      <nav className="w3-top">
+        <div className="w3-bar w3-white w3-padding w3-card">
+          <div className="w3-col s4">
+            <a href="/" className="w3-bar-item w3-button"><b>숨참고</b> <span className="w3-text-gray">보물 같은 중고 거래의 깊이 있는 발견</span></a>
           </div>
-          <div class="w3-col s4">
+          <div className="w3-col s4">
           {user ? (
-            <div class="w3-container">
+              <div className="w3-container w3-center">
               <form>
-                <fieldset class="w3-input w3-border w3-left" >
-                  <input class="w3-input w3-left" style={{width:'90%'}} type="text" placeholder="관심 있는 물건을 검색해보세요"></input><a href="#plans" class="w3-button"> <i class='fa fa-search'></i></a>
-                </fieldset>
+                  <input style={{ width: '90%', boxSizing: 'border-box', border: '1px solid #ccc', borderRadius: '4px', fontSize: '15px', backgroundColor: 'white', backgroundImage: `url(${searchIcon})`, backgroundPosition: '95% 12px', backgroundRepeat: 'no-repeat', padding: '12px 20px 12px 20px', transition: 'width 0.4s ease-in-out', }} type="text" placeholder="관심 있는 물건을 검색해보세요"></input><a href="#plans" className="w3-button"></a>
               </form>
             </div>
           ) : (
@@ -38,16 +43,16 @@ const Header = ({ user, setUser }) => {
             </>
           )} 
         </div>
-        <div class="w3-right">
+          <div className="w3-right">
         {user ? (
           <>
-          <a href="/mypage" class="w3-bar-item w3-button">안녕하세요, {user.username} 님!</a>
+                <a href="/mypage" className="w3-bar-item w3-button" onClick={handleGoToMyPage}>안녕하세요, {user.username} 님!</a>
           <button className="btn btn-secondary nav-link" onClick={handleLogout}>로그아웃</button>
           </>
           ) : (
             <>
-          <a href="/login" class="w3-bar-item w3-button">로그인</a>
-          <a href="/register" class="w3-bar-item w3-button">회원가입</a>
+                <a href="/login" className="w3-bar-item w3-button">로그인</a>
+                <a href="/register" className="w3-bar-item w3-button">회원가입</a>
               </>
           )}
           </div>
