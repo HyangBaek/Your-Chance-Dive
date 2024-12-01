34fa89d0a64d87e404c2668c2758d65bef3ce7ad
HyangBaek <22460026@st.yc.ac.kr>
2024-12-02 05:48:07 +0900

    18_frontend_header_Conditional hiding of search box

--- pjt_01_ucd/frontend/src/components/Header.js
+++ pjt_01_ucd/frontend/src/components/Header.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';  // useLocation 훅 임포트
 import style from './style/Header.module.css';
 import searchIcon from '../images/searchicon.png'; // 이미지 파일 임포트
 import Sideber from './Sideber';
 
 const Header = ({ user, setUser }) => {
   const navigate = useNavigate();
+  const location = useLocation();  // 현재 경로 정보 가져오기
+  // 경로가 "/search"일 경우 검색창(input)을 숨김
+  const isSearchPage = location.pathname.startsWith('/search');
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -30,11 +34,14 @@ const Header = ({ user, setUser }) => {
       <nav className="w3-top">
         <div className="w3-bar w3-white w3-padding w3-card">
           <div className="w3-col s4">
-            <a href="/" className="w3-bar-item w3-button"><b>숨참고</b> <span className="w3-text-gray">보물 같은 중고 거래의 깊이 있는 발견</span></a>
+            <a href="/" className="w3-bar-item w3-button"><b>숨참고</b> <span className="w3-text-gray w3-hide-small">보물 같은 중고 거래의 깊이 있는 발견</span></a>
           </div>
           <div className="w3-col s4">
             {user ? (
-              <div className="w3-container w3-center">
+              <>
+                {/* 검색창만 조건부로 숨김 */}
+                {!isSearchPage && (
+                  <div className="w3-container w3-center w3-hide-small">
                 <form
                   onSubmit={(e) => {
                     e.preventDefault();
@@ -47,9 +54,11 @@ const Header = ({ user, setUser }) => {
                     name="search"
                     placeholder="관심 있는 물건을 검색해보세요"
                     style={{ width: '90%', boxSizing: 'border-box', border: '1px solid #ccc', borderRadius: '4px', fontSize: '15px', backgroundColor: 'white', backgroundImage: `url(${searchIcon})`, backgroundPosition: '95% 12px', backgroundRepeat: 'no-repeat', padding: '12px 20px 12px 20px', transition: 'width 0.4s ease-in-out', }}
-                    />
-                </form>
-              </div>
+                      />
+                    </form>
+                  </div>
+                )}
+              </>
             ) : (
               <>
               </>
